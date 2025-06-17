55a9d74b97f9dd2fa16c661c849360f14532a9d9
westey <164392973+westey-m@users.noreply.github.com>
2025-06-17 12:23:39 +0100

    Add project and skeleton files.

---
+++ dotnet/src/Microsoft.Agents.CopilotStudio/CopilotStudioAgent.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.CopilotStudio;
+
+/// <summary>
+/// Represents a Copilot Studio agent in the cloud.
+/// </summary>
+public class CopilotStudioAgent : Agent
+{
+    /// <inheritdoc/>
+    public override AgentThread GetNewThread()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override Task<ChatResponse> RunAsync(IReadOnlyCollection<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override IAsyncEnumerable<ChatResponseUpdate> RunStreamingAsync(IReadOnlyCollection<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        throw new System.NotImplementedException();
+    }
+}
