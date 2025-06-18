f33dd0cb823e80b981491fb738025eb74f0da443
westey <164392973+westey-m@users.noreply.github.com>
2025-06-18 10:52:28 +0100

    Add CopilotStudioAgent implementation

---
+++ dotnet/src/Microsoft.Agents.CopilotStudio/ActivityProcessor.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Agents.CopilotStudio;
+
+/// <summary>
+/// Contains code to process <see cref="IActivity"/> responses from the Copilot Studio agent and convert them to <see cref="ChatMessage"/> objects.
+/// </summary>
+internal static class ActivityProcessor
+{
+    public static async IAsyncEnumerable<ChatMessage> ProcessActivityAsync(IAsyncEnumerable<IActivity> activities, ILogger logger)
+    {
+        await foreach (IActivity activity in activities.ConfigureAwait(false))
+        {
+            switch (activity.Type)
+            {
+                case "message":
+                    yield return
+                        new(ChatRole.Assistant, contents: [.. GetMessageItems(activity)])
+                        {
+                            RawRepresentation = activity,
+                        };
+                    break;
+                case "typing":
+                    yield return
+                        new(ChatRole.Assistant, contents: [new TextReasoningContent(activity.Text)])
+                        {
+                            RawRepresentation = activity,
+                        };
+                    break;
+                case "event":
+                    break;
+                default:
+                    logger.LogWarning("Unknown activity type '{ActivityType}' received.", activity.Type);
+                    break;
+            }
+        }
+    }
+
+    private static IEnumerable<AIContent> GetMessageItems(IActivity activity)
+    {
+        yield return new TextContent(activity.Text);
+        foreach (CardAction action in activity.SuggestedActions?.Actions ?? [])
+        {
+            yield return new TextContent(action.Title);
+        }
+    }
+}
--- dotnet/src/Microsoft.Agents.CopilotStudio/CopilotStudioAgent.cs
+++ dotnet/src/Microsoft.Agents.CopilotStudio/CopilotStudioAgent.cs
@@ -1,9 +1,15 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Agents.CopilotStudio.Client;
+using Microsoft.Agents.Core.Models;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Microsoft.Agents.CopilotStudio;
 
@@ -12,21 +18,103 @@ namespace Microsoft.Agents.CopilotStudio;
 /// </summary>
 public class CopilotStudioAgent : Agent
 {
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// The client used to interact with the Copilot Agent service.
+    /// </summary>
+    public CopilotClient Client { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CopilotStudioAgent"/> class.
+    /// </summary>
+    /// <param name="client">A client used to interact with the Copilot Agent service.</param>
+    /// <param name="loggerFactory">Optional logger factory to use for logging.</param>
+    public CopilotStudioAgent(CopilotClient client, ILoggerFactory? loggerFactory = null)
+    {
+        this.Client = client;
+
+        this._logger = (loggerFactory ?? NullLoggerFactory.Instance).CreateLogger<CopilotStudioAgent>();
+    }
+
     /// <inheritdoc/>
     public override AgentThread GetNewThread()
     {
-        throw new System.NotImplementedException();
+        return new CopilotStudioAgentThread();
     }
 
     /// <inheritdoc/>
-    public override Task<ChatResponse> RunAsync(IReadOnlyCollection<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    public override async Task<ChatResponse> RunAsync(
+        IReadOnlyCollection<ChatMessage> messages,
+        AgentThread? thread = null,
+        AgentRunOptions? options = null,
+        CancellationToken cancellationToken = default)
     {
-        throw new System.NotImplementedException();
+        // Ensure that we have a valid thread to work with.
+        CopilotStudioAgentThread copilotStudioAgentThread = base.ValidateOrCreateThreadType(thread, () => new CopilotStudioAgentThread());
+        if (copilotStudioAgentThread.Id is null)
+        {
+            // If the thread ID is null, we need to start a new conversation and set the thread ID accordingly.
+            copilotStudioAgentThread.Id = await this.StartNewConversationAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        // Invoke the Copilot Studio agent with the provided messages.
+        string question = string.Join("\n", messages.Select(m => m.Text));
+        var reponseMessages = ActivityProcessor.ProcessActivityAsync(this.Client.AskQuestionAsync(question, copilotStudioAgentThread.Id, cancellationToken), this._logger);
+
+        // Enumerate the response messages
+        var reponseMessagesList = new List<ChatMessage>();
+        await foreach (ChatMessage message in reponseMessages.ConfigureAwait(false))
+        {
+            // Add the message to the list
+            reponseMessagesList.Add(message);
+        }
+
+        return new ChatResponse(reponseMessagesList);
     }
 
     /// <inheritdoc/>
-    public override IAsyncEnumerable<ChatResponseUpdate> RunStreamingAsync(IReadOnlyCollection<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    public override async IAsyncEnumerable<ChatResponseUpdate> RunStreamingAsync(
+        IReadOnlyCollection<ChatMessage> messages,
+        AgentThread? thread = null,
+        AgentRunOptions? options = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        throw new System.NotImplementedException();
+        // Ensure that we have a valid thread to work with.
+        CopilotStudioAgentThread copilotStudioAgentThread = base.ValidateOrCreateThreadType(thread, () => new CopilotStudioAgentThread());
+        if (copilotStudioAgentThread.Id is null)
+        {
+            // If the thread ID is null, we need to start a new conversation and set the thread ID accordingly.
+            copilotStudioAgentThread.Id = await this.StartNewConversationAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        // Invoke the Copilot Studio agent with the provided messages.
+        string question = string.Join("\n", messages.Select(m => m.Text));
+        var reponseMessages = ActivityProcessor.ProcessActivityAsync(this.Client.AskQuestionAsync(question, copilotStudioAgentThread.Id, cancellationToken), this._logger);
+
+        // Enumerate the response messages
+        await foreach (ChatMessage message in reponseMessages.ConfigureAwait(false))
+        {
+            yield return new ChatResponseUpdate(message.Role, message.Contents);
+        }
+    }
+
+    private async Task<string> StartNewConversationAsync(CancellationToken cancellationToken)
+    {
+        string? conversationId = null;
+        await foreach (IActivity activity in this.Client.StartConversationAsync(emitStartConversationEvent: true, cancellationToken).ConfigureAwait(false))
+        {
+            if (activity.Conversation is not null)
+            {
+                conversationId = activity.Conversation.Id;
+            }
+        }
+
+        if (string.IsNullOrEmpty(conversationId))
+        {
+            throw new System.InvalidOperationException("Failed to start a new conversation.");
+        }
+
+        return conversationId;
     }
 }
