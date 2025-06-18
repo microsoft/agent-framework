d1d5a13e9bfa344bbed85610f91792af6a363f62
westey <164392973+westey-m@users.noreply.github.com>
2025-06-18 19:51:41 +0100

    Remove temp test.

--- dotnet/tests/CopilotStudio.IntegrationTests/CopilotStudioInvokeTests.cs
+++ dotnet/tests/CopilotStudio.IntegrationTests/CopilotStudioInvokeTests.cs
@@ -18,19 +18,4 @@ public class CopilotStudioInvokeTests() : RunAsyncTests<CopilotStudioFixture>(()
     {
         return Task.CompletedTask;
     }
-
-    [Fact]
-    public virtual async Task RunWithToolReturnsExpectedResultAsync()
-    {
-        // Arrange
-        var agent = this.Fixture.Agent;
-        var thread = agent.GetNewThread();
-
-        // Act
-        var chatResponse = await agent.RunAsync("Can you send a message to weslie steyn in teams, saying I'll be late for my meeting", thread);
-
-        // Assert
-        Assert.NotNull(chatResponse);
-        Assert.Single(chatResponse.Messages);
-    }
 }
