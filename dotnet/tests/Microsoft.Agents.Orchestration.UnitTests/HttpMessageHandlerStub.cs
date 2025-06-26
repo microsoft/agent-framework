ffb3d7a2c02e72ba44898edb39e5ad1083367bae
Chris Rickman <crickman@microsoft.com>
2025-06-26 11:11:49 -0700

    Simply http-stub

--- dotnet/tests/Microsoft.Agents.Orchestration.UnitTests/HttpMessageHandlerStub.cs
+++ dotnet/tests/Microsoft.Agents.Orchestration.UnitTests/HttpMessageHandlerStub.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,34 +9,10 @@ namespace Microsoft.Agents.Orchestration.UnitTest;
 
 internal sealed class HttpMessageHandlerStub : HttpMessageHandler
 {
-    public HttpRequestHeaders? RequestHeaders { get; private set; }
-
-    public HttpContentHeaders? ContentHeaders { get; private set; }
-
-    public byte[]? RequestContent { get; private set; }
-
-    public Uri? RequestUri { get; private set; }
-
-    public HttpMethod? Method { get; private set; }
-
     public Queue<HttpResponseMessage> ResponseQueue { get; } = new();
 
-    public byte[]? FirstMultipartContent { get; private set; }
-
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        this.Method = request.Method;
-        this.RequestUri = request.RequestUri;
-        this.RequestHeaders = request.Headers;
-        this.RequestContent = request.Content is null ? null : await request.Content.ReadAsByteArrayAsync(cancellationToken);
-
-        if (request.Content is MultipartContent multipartContent)
-        {
-            this.FirstMultipartContent = await multipartContent.First().ReadAsByteArrayAsync(cancellationToken);
-        }
-
-        this.ContentHeaders = request.Content?.Headers;
-
-        return this.ResponseQueue.Dequeue();
+        return Task.FromResult(this.ResponseQueue.Dequeue());
     }
 }
