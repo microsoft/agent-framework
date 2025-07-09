40b4f0e3216db0741e0eb877224bef51efa4a8c8
eavanvalkenburg <github@vanvalkenburg.eu>
2025-07-09 08:58:00 +0200

    removed src, updated test naming

---
+++ python/tests/unit/test_cross_package.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+
+def test_openai():
+    try:
+        from agent_framework.openai import __version__
+    except ImportError:
+        __version__ = None
+    assert __version__ is not None
--- python/tests/unit/test_openai.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from agent_framework.openai import __version__
-
-
-def test_version():
-    assert __version__ is not None
