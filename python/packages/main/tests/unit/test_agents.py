512565fa69b6534681e70832b2fe80f883d05e72
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2025-07-11 10:45:50 -0700

    Small fix

--- python/packages/main/tests/unit/test_agents.py
+++ python/packages/main/tests/unit/test_agents.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from collections.abc import AsyncIterable, Sequence
-from typing import Any, MutableSequence
+from typing import Any
+from collections.abc import MutableSequence
 from uuid import uuid4
 
 from pytest import fixture, raises
