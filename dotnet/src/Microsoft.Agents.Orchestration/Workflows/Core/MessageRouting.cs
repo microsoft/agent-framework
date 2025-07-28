3811ae97485b88ee9e2529cb1f7a9f5d8f7904bc
Jacob Alber <jaalber@microsoft.com>
2025-07-28 18:59:59 -0400

    feat: Define IExecutionContext and Events

--- dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/MessageRouting.cs
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/MessageRouting.cs
@@ -17,6 +17,27 @@ using HandlerInfosT =
 namespace Microsoft.Agents.Orchestration.Workflows.Core;
 
 /// <summary>
+/// This attribute indicates that a message handler streams messages during its execution.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public sealed class StreamsMessageAttribute : Attribute
+{
+    /// <summary>
+    /// The type of the message that the handler yields.
+    /// </summary>
+    public Type Type { get; }
+
+    /// <summary>
+    /// Indicates that the message handler yields streaming messages during the course of execution.
+    /// </summary>
+    public StreamsMessageAttribute(Type type)
+    {
+        // This attribute is used to mark executors that yield messages.
+        this.Type = type ?? throw new ArgumentNullException(nameof(type), "Type cannot be null.");
+    }
+}
+
+/// <summary>
 /// This class represents the result of a call to a <see cref="IMessageHandler{TMessage}"/>
 /// or <see cref="IMessageHandler{TMessage,TResult}"/>.
 /// </summary>
