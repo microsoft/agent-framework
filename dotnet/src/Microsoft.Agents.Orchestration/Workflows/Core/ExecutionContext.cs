30101799ee4e54dbad2a7578a191820d985c3043
Jacob Alber <jaalber@microsoft.com>
2025-07-28 16:08:29 -0400

    feat: Define Workflow and Executor APIs

---
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/ExecutionContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.Agents.Orchestration.Workflows.Core;
+
+/// <summary>
+/// Provides services for <see cref="Executor"/> subclasses.
+/// </summary>
+public interface IExecutionContext
+{
+    /// <summary>
+    /// .
+    /// </summary>
+    /// <returns></returns>
+    Task MagicAsync();
+}
