a3117c75b7e4464819d0602e38769d75df37b195
Jacob Alber <jaalber@microsoft.com>
2025-07-28 18:59:59 -0400

    feat: Define IExecutionContext and Events

---
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/Events.cs
@@ -0,0 +1,152 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.Agents.Orchestration.Workflows.Core;
+
+/// <summary>
+/// .
+/// </summary>
+public record WorkflowEvent(object? Data = null);
+
+/// <summary>
+/// .
+/// </summary>
+public record WorkflowStartedEvent : WorkflowEvent;
+
+/// <summary>
+/// .
+/// </summary>
+public record WorkflowCompletedEvent : WorkflowEvent;
+
+/// <summary>
+/// .
+/// </summary>
+public record ExecutorEvent : WorkflowEvent
+{
+    /// <summary>
+    /// The identifier of the executor that generated this event.
+    /// </summary>
+#if NET9_0_OR_GREATER
+    required
+#endif
+    public string ExecutorId
+    { get; init; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorEvent(string executorId, object? data = null) : base(data)
+    {
+        this.ExecutorId = executorId ?? throw new ArgumentNullException(nameof(executorId), "Executor ID cannot be null.");
+    }
+
+#if NET9_0_OR_GREATER
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorEvent()
+    { }
+#endif
+}
+
+/// <summary>
+/// .
+/// </summary>
+public record ExecutorInvokeEvent : ExecutorEvent
+{
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorInvokeEvent(string executorId, object? data = null) : base(data)
+    {
+        this.ExecutorId = executorId ?? throw new ArgumentNullException(nameof(executorId), "Executor ID cannot be null.");
+    }
+
+#if NET9_0_OR_GREATER
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorInvokeEvent()
+    { }
+#endif
+}
+
+/// <summary>
+/// .
+/// </summary>
+public record ExecutorCompleteEvent : ExecutorEvent
+{
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorCompleteEvent(string executorId, object? data = null) : base(data)
+    {
+        this.ExecutorId = executorId ?? throw new ArgumentNullException(nameof(executorId), "Executor ID cannot be null.");
+    }
+
+#if NET9_0_OR_GREATER
+    /// <summary>
+    /// .
+    /// </summary>
+    public ExecutorCompleteEvent()
+    { }
+#endif
+}
+
+// TODO: This is a placeholder for streaming chat message content.
+/// <summary>
+/// .
+/// </summary>
+public class StreamingChatMessageContent
+{ }
+
+/// <summary>
+/// .
+/// </summary>
+public record AgentRunStreamingEvent : ExecutorEvent
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AgentRunStreamingEvent"/> class.
+    /// </summary>
+    /// <param name="executorId">The identifier of the executor that generated this event.</param>
+    /// <param name="content"></param>
+    public AgentRunStreamingEvent(string executorId, StreamingChatMessageContent? content = null) : base(executorId, data: content)
+    {
+        this.Content = content;
+    }
+
+    /// <summary>
+    /// Gets the content of the streaming chat message.
+    /// </summary>
+    public StreamingChatMessageContent? Content { get; }
+}
+
+// TODO: This is a placeholder for non-streaming chat message content.
+/// <summary>
+/// .
+/// </summary>
+public class ChatMessageContent
+{
+}
+
+/// <summary>
+/// .
+/// </summary>
+public record AgentRunEvent : ExecutorEvent
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AgentRunEvent"/> class.
+    /// </summary>
+    /// <param name="executorId">The identifier of the executor that generated this event.</param>
+    /// <param name="content"></param>
+    public AgentRunEvent(string executorId, ChatMessageContent? content = null) : base(executorId, data: content)
+    {
+        this.Content = content;
+    }
+
+    /// <summary>
+    /// Gets the content of the chat message.
+    /// </summary>
+    public ChatMessageContent? Content { get; }
+}
--- dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/ExecutionContext.cs
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Core/ExecutionContext.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Microsoft.Agents.Orchestration.Workflows.Core;
@@ -10,8 +11,45 @@ namespace Microsoft.Agents.Orchestration.Workflows.Core;
 public interface IExecutionContext
 {
     /// <summary>
-    /// .
+    /// Send a message from the executor to the context.
     /// </summary>
-    /// <returns></returns>
-    Task MagicAsync();
+    /// <param name="sourceId">The id of the sender of the message.</param>
+    /// <param name="message">The message to be sent.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+    ValueTask SendMessageAsync(string sourceId, object message);
+
+    /// <summary>
+    /// Drain all messages from the context.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation, containing
+    /// a dictionary mapping executor IDs to lists of messages.</returns>
+    ValueTask<Dictionary<string, List<object>>> DrainMessagesAsync();
+
+    /// <summary>
+    /// Check if there are any message in the context.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation, containing
+    /// <c>true</c> if there are messages. <c>false</c> if there are not.</returns>
+    ValueTask<bool> HasMessagesAsync();
+
+    /// <summary>
+    /// Add an event to the execution context.
+    /// </summary>
+    /// <param name="workflowEvent">The event to be added.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+    ValueTask AddEventAsync(WorkflowEvent workflowEvent);
+
+    /// <summary>
+    /// Drain all events from the context.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation, containing
+    /// a list of all events.</returns>
+    ValueTask<List<WorkflowEvent>> DrainEventsAsync();
+
+    /// <summary>
+    /// Check if there are any events in the context.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation, containing
+    /// <c>true</c> if there are events. <c>false</c> if there are not.</returns>
+    ValueTask<bool> HasEventsAsync();
 }
