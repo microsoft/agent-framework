610b5e613ef0d60ea610d467163ae324bb9c4df0
Jacob Alber <jaalber@microsoft.com>
2025-07-28 19:00:15 -0400

    feat: Simple Workflow Demos

---
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Sample/02_Simple_Workflow_Sequential.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.Agents.Orchestration.Workflows.Core;
+
+namespace Microsoft.Agents.Orchestration.Workflows.Sample;
+
+internal static class Step2EntryPoint
+{
+    public static ValueTask RunAsync()
+    {
+        UppercaseExecutor uppercase = new();
+        ReverseTextExecutor reverse = new();
+
+        WorkflowBuilder builder = new(uppercase);
+        builder.AddEdge(uppercase, reverse);
+
+        Workflow<string> workflow = builder.Build<string>();
+        // async foreach (var event in workflow.RunAsync("hello world"))
+        //    await Console.Out.WriteLineAsync(event);
+
+        return CompletedValueTaskSource.Completed;
+    }
+}
+
+internal class UppercaseExecutor : Executor, IMessageHandler<string, string>
+{
+    public ValueTask<string> HandleAsync(string message, IExecutionContext context)
+    {
+        return CompletedValueTaskSource.FromResult(message.ToUpperInvariant());
+    }
+}
+
+internal class ReverseTextExecutor : Executor, IMessageHandler<string, string>
+{
+    public ValueTask<string> HandleAsync(string message, IExecutionContext context)
+    {
+        char[] charArray = message.ToCharArray();
+        System.Array.Reverse(charArray);
+        return CompletedValueTaskSource.FromResult(new string(charArray));
+    }
+}
---
+++ dotnet/src/Microsoft.Agents.Orchestration/Workflows/Sample/02b_Simple_Workflow_Loop.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.Agents.Orchestration.Workflows.Core;
+
+namespace Microsoft.Agents.Orchestration.Workflows.Sample;
+
+internal static class Step2bEntryPoint
+{
+    public static ValueTask RunAsync()
+    {
+        GuessNumberExecutor guessNumber = new(1, 100);
+        JudgeExecutor judge = new(42); // Let's say the target number is 42
+
+        Workflow<NumberSignal> workflow = new WorkflowBuilder(guessNumber)
+            .AddLoop(guessNumber, judge)
+            .Build<NumberSignal>();
+
+        // async foreach (var event in workflow.RunAsync(NumberSignal.Init))
+        //     await Console.Out.WriteLineAsync(event);
+
+        return CompletedValueTaskSource.Completed;
+    }
+}
+
+internal enum NumberSignal
+{
+    Init,
+    Above,
+    Below,
+    Matched
+}
+
+internal class GuessNumberExecutor : Executor, IMessageHandler<NumberSignal, int>
+{
+    public int LowerBound { get; private set; }
+    public int UpperBound { get; private set; }
+
+    public GuessNumberExecutor(int lowerBound, int upperBound)
+    {
+        this.LowerBound = lowerBound;
+        this.UpperBound = upperBound;
+    }
+
+    private int NextGuess => (this.LowerBound + this.UpperBound) / 2;
+
+    private int _currGuess = -1;
+    public async ValueTask<int> HandleAsync(NumberSignal message, IExecutionContext context)
+    {
+        switch (message)
+        {
+            case NumberSignal.Matched:
+                await context.AddEventAsync(new WorkflowCompletedEvent { Data = $"Guessed the number: {this._currGuess}" })
+                             .ConfigureAwait(false);
+                break;
+
+            case NumberSignal.Above:
+                this.UpperBound = this._currGuess - 1;
+                break;
+            case NumberSignal.Below:
+                this.LowerBound = this._currGuess + 1;
+                break;
+        }
+
+        return this._currGuess = this.NextGuess;
+    }
+}
+
+internal class JudgeExecutor : Executor, IMessageHandler<int, NumberSignal>
+{
+    private readonly int _targetNumber;
+
+    public JudgeExecutor(int targetNumber)
+    {
+        this._targetNumber = targetNumber;
+    }
+
+    public ValueTask<NumberSignal> HandleAsync(int message, IExecutionContext context)
+    {
+        if (message == this._targetNumber)
+        {
+            return CompletedValueTaskSource.FromResult(NumberSignal.Matched);
+        }
+        else if (message < this._targetNumber)
+        {
+            return CompletedValueTaskSource.FromResult(NumberSignal.Below);
+        }
+        else
+        {
+            return CompletedValueTaskSource.FromResult(NumberSignal.Above);
+        }
+    }
+}
