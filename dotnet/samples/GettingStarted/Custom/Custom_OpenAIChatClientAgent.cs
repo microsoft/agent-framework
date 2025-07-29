43f303fb8eafd4395efc01bc98ce378fdd039638
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-07-29 19:50:53 +0100

    Add OpenAI chat completion based implementation of AIAgent

---
+++ dotnet/samples/GettingStarted/Custom/Custom_OpenAIChatClientAgent.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Shared.Samples;
+using OpenAI;
+using OpenAI.Chat;
+
+namespace Custom;
+
+/// <summary>
+/// End-to-end sample showing how to use a custom <see cref="OpenAIChatClientAgent"/>.
+/// </summary>
+public sealed class Custom_OpenAIChatClientAgent(ITestOutputHelper output) : AgentSample(output)
+{
+    /// <summary>
+    /// This will create an instance of <see cref="MyOpenAIChatClientAgent"/> and run it.
+    /// </summary>
+    [Fact]
+    public async Task RunCustomChatClientAgent()
+    {
+        var openAIClient = new OpenAIClient(TestConfiguration.OpenAI.ApiKey);
+        var model = TestConfiguration.OpenAI.ChatModelId;
+
+        var agent = new MyOpenAIChatClientAgent(openAIClient, model);
+
+        var chatMessage = new UserChatMessage("Tell me a joke about a pirate.");
+        var chatCompletion = await agent.RunAsync(chatMessage);
+
+        Console.WriteLine(chatCompletion.Content.Last().Text);
+    }
+}
+
+public class MyOpenAIChatClientAgent : OpenAIChatClientAgent
+{
+    private const string JokerName = "Joker";
+    private const string JokerInstructions = "You are good at telling jokes.";
+
+    public MyOpenAIChatClientAgent(OpenAIClient client, string model, ILoggerFactory? loggerFactory = null) :
+        base(client, model, instructions: JokerInstructions, name: JokerName, loggerFactory: loggerFactory)
+    {
+    }
+}
---
+++ dotnet/samples/GettingStarted/External/MEAI.OpenAI/OpenAIChatClientAgent.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.AI.Agents;
+using Microsoft.Extensions.Logging;
+using Microsoft.Shared.Diagnostics;
+using OpenAI.Chat;
+using ChatMessage = OpenAI.Chat.ChatMessage;
+
+namespace OpenAI;
+
+/// <summary>
+/// OpenAI chat completion based implementation of <see cref="AIAgent"/>.
+/// </summary>
+public class OpenAIChatClientAgent : AIAgent
+{
+    private readonly ChatClientAgent _chatClientAgent;
+
+    public OpenAIChatClientAgent(OpenAIClient client, string model, string? instructions = null, string? name = null, string? description = null, ILoggerFactory? loggerFactory = null)
+    {
+        Throw.IfNull(client);
+        Throw.IfNull(model);
+
+        var chatClient = client.GetChatClient(model).AsIChatClient();
+        this._chatClientAgent = new(
+            chatClient,
+            new ChatClientAgentOptions()
+            {
+                Name = name,
+                Description = description,
+                Instructions = instructions,
+            },
+            loggerFactory);
+    }
+
+    public OpenAIChatClientAgent(OpenAIClient client, string model, ChatClientAgentOptions options, ILoggerFactory? loggerFactory = null)
+    {
+        Throw.IfNull(client);
+        Throw.IfNull(model);
+
+        var chatClient = client.GetChatClient(model).AsIChatClient();
+        this._chatClientAgent = new(chatClient, options, loggerFactory);
+    }
+
+    public async Task<ChatCompletion> RunAsync(IEnumerable<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        var response = await this.RunAsync([.. messages.AsChatMessages()], thread, options, cancellationToken);
+
+        var chatCompletion = response.AsChatCompletion();
+        return chatCompletion;
+    }
+
+    /// <inheritdoc/>
+    public override AgentThread GetNewThread()
+    {
+        return this._chatClientAgent.GetNewThread();
+    }
+
+    /// <inheritdoc/>
+    public override Task<AgentRunResponse> RunAsync(IReadOnlyCollection<Microsoft.Extensions.AI.ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return this._chatClientAgent.RunAsync(messages, thread, options, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public override IAsyncEnumerable<AgentRunResponseUpdate> RunStreamingAsync(IReadOnlyCollection<Microsoft.Extensions.AI.ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return this._chatClientAgent.RunStreamingAsync(messages, thread, options, cancellationToken);
+    }
+}
