e33a6b78f1bf7bd8334b0152fdbf97628ed9be05
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-07-29 17:54:44 +0100

    Sample showing how to get an AI agent for Foundry SDK

--- dotnet/samples/GettingStarted/Providers/AIAgent_With_AzureAIAgentsPersistent.cs
+++ dotnet/samples/GettingStarted/Providers/AIAgent_With_AzureAIAgentsPersistent.cs
@@ -19,7 +19,44 @@ public sealed class AIAgent_With_AzureAIAgentsPersistent(ITestOutputHelper outpu
     private const string JokerInstructions = "You are good at telling jokes.";
 
     [Fact]
-    public async Task RunWithAzureAIAgentsPersistent()
+    public async Task GetWithAzureAIAgentsPersistent()
+    {
+        // Get a client to create server side agents with.
+        var persistentAgentsClient = new PersistentAgentsClient(TestConfiguration.AzureAI.Endpoint, new AzureCliCredential());
+
+        // Create a service side persistent agent.
+        var persistentAgent = await persistentAgentsClient.Administration.CreateAgentAsync(
+            model: TestConfiguration.AzureAI.DeploymentName!,
+            name: JokerName,
+            instructions: JokerInstructions);
+
+        // Get a server side agent.
+        AIAgent agent = await persistentAgentsClient.GetAIAgentAsync(persistentAgent.Value.Id);
+
+        // Start a new thread for the agent conversation.
+        AgentThread thread = agent.GetNewThread();
+
+        // Respond to user input
+        await RunAgentAsync("Tell me a joke about a pirate.");
+        await RunAgentAsync("Now add some emojis to the joke.");
+
+        // Local function to run agent and display the conversation messages for the thread.
+        async Task RunAgentAsync(string input)
+        {
+            Console.WriteLine(input);
+
+            var response = await agent.RunAsync(input, thread);
+
+            Console.WriteLine(response);
+        }
+
+        // Cleanup
+        await persistentAgentsClient.Threads.DeleteThreadAsync(thread.Id);
+        await persistentAgentsClient.Administration.DeleteAgentAsync(agent.Id);
+    }
+
+    [Fact]
+    public async Task CreateWithAzureAIAgentsPersistent()
     {
         // Get a client to create server side agents with.
         var persistentAgentsClient = new PersistentAgentsClient(TestConfiguration.AzureAI.Endpoint, new AzureCliCredential());
