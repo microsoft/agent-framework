eed3a7671c98ad11e6b94eeb637e92538b46890c
Jacob Alber <jaalber@microsoft.com>
2025-08-05 14:01:06 -0400

    refactor: Break EdgeRunner file into per-type files

---
+++ dotnet/src/Microsoft.Agents.Workflow/Execution/FanOutEdgeRunner.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Agents.Workflows.Core;
+
+namespace Microsoft.Agents.Workflows.Execution;
+
+internal class FanOutEdgeRunner(IRunnerContext runContext, FanOutEdgeData edgeData) :
+    EdgeRunner<FanOutEdgeData>(runContext, edgeData)
+{
+    private Dictionary<string, IWorkflowContext> BoundContexts { get; }
+        = edgeData.SinkIds.ToDictionary(
+            sinkId => sinkId,
+            sinkId => runContext.Bind(sinkId));
+
+    public async ValueTask<IEnumerable<CallResult?>> ChaseAsync(object message)
+    {
+        List<string> targets =
+            this.EdgeData.Partitioner == null
+                ? this.EdgeData.SinkIds
+                : this.EdgeData.Partitioner(message, this.BoundContexts.Count).Select(i => this.EdgeData.SinkIds[i]).ToList();
+
+        CallResult?[] result = await Task.WhenAll(targets.Select(ProcessTargetAsync)).ConfigureAwait(false);
+        return result.Where(r => r is not null);
+
+        async Task<CallResult?> ProcessTargetAsync(string targetId)
+        {
+            Executor executor = await this.RunContext.EnsureExecutorAsync(targetId)
+                                                     .ConfigureAwait(false);
+
+            MessageRouter router = executor.MessageRouter;
+            if (router.CanHandle(message))
+            {
+                return await router.RouteMessageAsync(message, this.BoundContexts[targetId])
+                                   .ConfigureAwait(false);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ dotnet/src/Microsoft.Agents.Workflow/Execution/InputEdgeRuner.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.Agents.Workflows.Core;
+
+namespace Microsoft.Agents.Workflows.Execution;
+
+internal class InputEdgeRuner(IRunnerContext runContext, string sinkId)
+    : EdgeRunner<string>(runContext, sinkId)
+{
+    public IWorkflowContext WorkflowContext { get; } = runContext.Bind(sinkId);
+
+    private async ValueTask<MessageRouter> FindRouterAsync()
+    {
+        Executor sink = await this.RunContext.EnsureExecutorAsync(this.EdgeData)
+                                             .ConfigureAwait(false);
+
+        return sink.MessageRouter;
+    }
+
+    public async ValueTask<CallResult?> ChaseAsync(object message)
+    {
+        MessageRouter router = await this.FindRouterAsync().ConfigureAwait(false);
+        if (router.CanHandle(message))
+        {
+            return await router.RouteMessageAsync(message, this.WorkflowContext)
+                                             .ConfigureAwait(false);
+        }
+
+        // TODO: Throw instead?
+
+        return null;
+    }
+}
