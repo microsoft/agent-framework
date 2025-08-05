ea1476136be9408cfd6c2b8e510badabc738e33f
Jacob Alber <jaalber@microsoft.com>
2025-08-05 14:01:06 -0400

    refactor: Break EdgeRunner file into per-type files

---
+++ dotnet/src/Microsoft.Agents.Workflow/Execution/FanInEdgeRunner.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Agents.Workflows.Core;
+
+namespace Microsoft.Agents.Workflows.Execution;
+
+internal class FanInEdgeRunner(IRunnerContext runContext, FanInEdgeData edgeData) :
+    EdgeRunner<FanInEdgeData>(runContext, edgeData)
+{
+    private IWorkflowContext BoundContext { get; } = runContext.Bind(edgeData.SinkId);
+
+    public FanInEdgeState CreateState() => new(this.EdgeData);
+
+    public async ValueTask<CallResult?> ChaseAsync(string sourceId, object message, FanInEdgeState state)
+    {
+        IEnumerable<object>? releasedMessages = state.ProcessMessage(sourceId, message);
+        if (releasedMessages is null)
+        {
+            // Not ready to process yet.
+            return null;
+        }
+
+        Executor sink = await this.RunContext.EnsureExecutorAsync(this.EdgeData.SinkId)
+                                             .ConfigureAwait(false);
+
+        MessageRouter router = sink.MessageRouter;
+        if (router.CanHandle(message))
+        {
+            return await router.RouteMessageAsync(message, this.BoundContext)
+                                             .ConfigureAwait(false);
+        }
+        return null;
+    }
+}
