2b55bd31b66b20ce6e53a7e640c884f719d6734f
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-09-03 10:30:24 +0100

    Getting started samples which use OpenAI exchange types

---
+++ dotnet/src/Microsoft.Extensions.AI.Agents.OpenAI/ChatCompletion/StreamingUpdatePipelineResponse.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ClientModel.Primitives;
+
+namespace Microsoft.Extensions.AI.Agents.OpenAI.ChatCompletion;
+
+internal sealed class StreamingUpdatePipelineResponse : PipelineResponse
+{
+    public override int Status => throw new NotImplementedException();
+
+    public override string ReasonPhrase => throw new NotImplementedException();
+
+    public override Stream? ContentStream { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override BinaryData Content => throw new NotImplementedException();
+
+    protected override PipelineResponseHeaders HeadersCore => throw new NotImplementedException();
+
+    public override BinaryData BufferContent(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<BinaryData> BufferContentAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal StreamingUpdatePipelineResponse(IAsyncEnumerable<AgentRunResponseUpdate> updates)
+    {
+        this._updates = updates;
+    }
+
+    private readonly IAsyncEnumerable<AgentRunResponseUpdate> _updates;
+}
