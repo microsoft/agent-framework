a2abbda8d8985da26752edc0cf140e13927c59df
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-09-04 15:24:26 +0100

    Start to add declarative samples

---
+++ dotnet/samples/GettingStarted/DeclarativeAgents/DeclarativeAgent_ChatClient/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+// This sample shows how to create and use a simple AI agent with Azure OpenAI as the backend.
+
+using System;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.AI.Agents;
+using Microsoft.Extensions.AI.Agents.ChatCompletion;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+// Create the chat client
+IChatClient chatClient = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new AzureCliCredential())
+     .GetChatClient(deploymentName)
+     .AsIChatClient();
+
+// Define the agent using a YAML definition.
+var text =
+    """
+    type: chat_client_agent
+    name: Joker
+    description: Joker Agent
+    instructions: You are good at telling jokes.
+    """;
+
+// Create the agent from the YAML definition.
+var agentFactory = new ChatClientAgentFactory();
+var agent = await agentFactory.CreateAgentFromYamlAsync(text, new() { ChatClient = chatClient });
+
+// Invoke the agent and output the text result.
+Console.WriteLine(await agent!.RunAsync("Tell me a joke about a pirate."));
+
+// Invoke the agent with streaming support.
+await foreach (var update in agent!.RunStreamingAsync("Tell me a joke about a pirate."))
+{
+    Console.WriteLine(update);
+}
---
+++ dotnet/src/Microsoft.Extensions.AI.Agents.AzureAI/AzureOpenAIAgentFactory.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Azure.AI.OpenAI;
+
+namespace Microsoft.Extensions.AI.Agents.AzureAI;
+
+/// <summary>
+/// Provides an <see cref="AgentFactory"/> which creates instances of <see cref="ChatClientAgent"/> using a <see cref="AzureOpenAIClient"/>.
+/// </summary>
+public sealed class AzureOpenAIAgentFactory : AgentFactory
+{
+    /// <summary>
+    /// The type of the chat client agent.
+    /// </summary>
+    public const string AzureOpenAIAgentType = "azureopenai_agent";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AzureOpenAIAgentFactory"/> class.
+    /// </summary>
+    public AzureOpenAIAgentFactory()
+        : base([AzureOpenAIAgentType])
+    {
+    }
+
+    /// <inheritdoc/>
+    public override Task<AIAgent?> TryCreateAsync(AgentDefinition agentDefinition, AgentCreationOptions agentCreationOptions, CancellationToken cancellationToken = default)
+    {
+        //Throw.IfNull(agentDefinition);
+
+        ChatClientAgent? agent = null;
+        if (this.IsSupported(agentDefinition))
+        {
+            IChatClient? chatClient = null;
+
+            var client = agentCreationOptions.ServiceProvider?.GetService(typeof(AzureOpenAIClient)) as AzureOpenAIClient;
+            if (client is not null)
+            {
+                //agentDefinition.Model.Connection?.Options
+                //chatClient = client.GetChatClient(deploymentName).AsIChatClient();
+            }
+
+            if (chatClient is not null)
+            {
+                agent = new ChatClientAgent(chatClient, new ChatClientAgentOptions(), agentCreationOptions.LoggerFactory);
+            }
+        }
+
+        return Task.FromResult<AIAgent?>(agent);
+    }
+}
