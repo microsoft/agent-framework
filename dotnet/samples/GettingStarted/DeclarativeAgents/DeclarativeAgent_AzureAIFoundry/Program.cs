ebfc47ecee3fdef8acf55db334600d2124c51b50
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-09-05 13:59:30 +0100

    Start work on declarative support for Foundry agents

---
+++ dotnet/samples/GettingStarted/DeclarativeAgents/DeclarativeAgent_AzureAIFoundry/Program.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+// This sample shows how to create and use a simple AI agent with Azure OpenAI as the backend.
+
+using System;
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.AI.Agents;
+using Microsoft.Extensions.AI.Agents.AzureAI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+// Create a dictionary with your fixed properties  
+var inMemorySettings = new Dictionary<string, string?>
+{
+    { "AzureOpenAI:Endpoint", endpoint },
+    { "AzureOpenAI:DeploymentName", deploymentName },
+    { "AzureOpenAI:ModelId", deploymentName },
+};
+
+// Build the IConfiguration instance to allow for variable substitution in the YAML definition
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(inMemorySettings)
+    .Build();
+
+// Set up dependency injection to provide the TokenCredential implementation
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddTransient<TokenCredential, AzureCliCredential>();
+IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+// Define the agent using a YAML definition.
+var text =
+    """
+    type: azure_openai_agent
+    name: Joker
+    description: Joker Agent
+    instructions: You are good at telling jokes.
+    model:
+      id: ${AzureOpenAI:ModelId}
+      connection:
+        type: azure_openai
+        provider: azure_openai
+        endpoint: ${AzureOpenAI:Endpoint}
+        options:
+          deployment_name: ${AzureOpenAI:DeploymentName}
+    """;
+
+// Create the agent from the YAML definition.
+var agentFactory = new AzureOpenAIAgentFactory();
+var creationOptions = new AgentCreationOptions()
+{
+    Configuration = configuration,
+    ServiceProvider = serviceProvider,
+};
+var agent = await agentFactory.CreateAgentFromYamlAsync(text, creationOptions);
+
+// Invoke the agent and output the text result.
+Console.WriteLine(await agent!.RunAsync("Tell me a joke about a pirate."));
+
+// Invoke the agent with streaming support.
+await foreach (var update in agent!.RunStreamingAsync("Tell me a joke about a pirate."))
+{
+    Console.WriteLine(update);
+}
---
+++ dotnet/src/Microsoft.Extensions.AI.Agents.AzureAI/AzureFoundryAgentFactory.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Azure.AI.Agents.Persistent;
+
+namespace Microsoft.Extensions.AI.Agents.AzureAI;
+
+/// <summary>
+/// Provides an <see cref="AgentFactory"/> which creates instances of <see cref="ChatClientAgent"/> using a <see cref="PersistentAgentsClient"/>.
+/// </summary>
+public sealed class AzureFoundryAgentFactory : AgentFactory
+{
+    /// <summary>
+    /// The type of the chat client agent.
+    /// </summary>
+    public const string AzureFoundryAgentType = "azure_foundry_agent";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AzureOpenAIAgentFactory"/> class.
+    /// </summary>
+    public AzureFoundryAgentFactory()
+        : base([AzureFoundryAgentType])
+    {
+    }
+
+    /// <inheritdoc/>
+    public override Task<AIAgent?> TryCreateAsync(AgentDefinition agentDefinition, AgentCreationOptions agentCreationOptions, CancellationToken cancellationToken = default)
+    {
+        //Throw.IfNull(agentDefinition);
+
+        ChatClientAgent? agent = null;
+        /*
+        if (this.IsSupported(agentDefinition))
+        {
+            IChatClient? chatClient = null;
+
+            var deploymentName = agentDefinition.Model.Connection?.Options?["deployment_name"] as string;
+            if (string.IsNullOrEmpty(deploymentName))
+            {
+                throw new InvalidOperationException("The deployment_name must be specified in the agent definition model connection options to create a ChatClient.");
+            }
+
+            var client = agentCreationOptions.ServiceProvider?.GetService(typeof(AzureOpenAIClient)) as AzureOpenAIClient;
+            if (client is not null)
+            {
+                chatClient = client.GetChatClient(deploymentName).AsIChatClient();
+            }
+            else
+            {
+                var endpoint = agentDefinition.Model.Connection?.Endpoint;
+                if (string.IsNullOrEmpty(endpoint))
+                {
+                    throw new InvalidOperationException("The endpoint must be specified in the agent definition model connection to create an AzureOpenAIClient.");
+                }
+
+                var credential = agentCreationOptions.ServiceProvider?.GetService(typeof(TokenCredential)) as TokenCredential;
+                if (credential is null)
+                {
+                    throw new InvalidOperationException("A TokenCredential must be registered in the service provider to create an AzureOpenAIClient.");
+                }
+
+                chatClient = new AzureOpenAIClient(
+                    new Uri(endpoint),
+                    credential)
+                     .GetChatClient(deploymentName)
+                     .AsIChatClient();
+            }
+
+            agent = new ChatClientAgent(chatClient, new ChatClientAgentOptions(), agentCreationOptions.LoggerFactory);
+        }
+        */
+
+        return Task.FromResult<AIAgent?>(agent);
+    }
+}
