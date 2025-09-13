1f8507cafc1dc56315d8b1d74b772de18050b45c
Yuge Zhang <scottyugochang@gmail.com>
2025-09-13 19:59:24 +0800

    try to reproduce

--- python/samples/getting_started/agents/agent_lightning/lightning_client.py
+++ python/samples/getting_started/agents/agent_lightning/lightning_client.py
@@ -46,7 +46,8 @@ class Tau2Agent(LitAgent):
 
         assistant_config = AgentConfiguration(
             model=main_llm.model,
-            temperature=main_llm.sampling_parameters["temperature"],
+            temperature=None,
+            # temperature=main_llm.sampling_parameters["temperature"],
             base_url=main_llm.endpoint,
             # model="gpt-4.1",
             # temperature=0.0,
@@ -57,14 +58,16 @@ class Tau2Agent(LitAgent):
         )
         user_config = AgentConfiguration(
             model="gpt-4.1",
-            temperature=0.0,
+            temperature=None,
+            # temperature=0.0,
             base_url=proxy_base_url,
             api_key=proxy_api_key,
             sliding_window=30000,  # long sliding window for user simulator
         )
         judge_config = AgentConfiguration(
             model="gpt-4o-mini",
-            temperature=0.0,
+            temperature=None,
+            # temperature=0.0,
             base_url=proxy_base_url,
             api_key=proxy_api_key,
             sliding_window=0,  # Not used for judge
