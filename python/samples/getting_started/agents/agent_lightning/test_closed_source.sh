255364c6a7d54e51418295c3b7c5c66cc48cb46b
Yuge Zhang <scottyugochang@gmail.com>
2025-09-14 14:41:51 +0800

    .

--- python/samples/getting_started/agents/agent_lightning/test_closed_source.sh
+++ python/samples/getting_started/agents/agent_lightning/test_closed_source.sh
@@ -1,9 +1,11 @@
 set -x
-dotenv run python test.py --assistant Qwen2.5-1.5B-Instruct --user gpt-4.1 --assistant-sliding-window 4000
-dotenv run python test.py --assistant gpt-4.1 --user gpt-4.1 --assistant-sliding-window 4000
-dotenv run python test.py --assistant gpt-4.1 --user gpt-4.1 --assistant-sliding-window 28000
-dotenv run python test.py --assistant gpt-4.1 --user gpt-4o-mini --assistant-sliding-window 28000
-dotenv run python test.py --assistant gpt-4o-mini --user gpt-4.1 --assistant-sliding-window 28000
-dotenv run python test.py --assistant gpt-4.1-mini --user gpt-4.1 --assistant-sliding-window 28000
+# dotenv run python test.py --assistant Qwen2.5-1.5B-Instruct --user gpt-4.1 --assistant-sliding-window 4000
+# dotenv run python test.py --assistant gpt-4.1 --user gpt-4.1 --assistant-sliding-window 4000
+# dotenv run python test.py --assistant gpt-4.1 --user gpt-4.1 --assistant-sliding-window 28000
+# dotenv run python test.py --assistant gpt-4.1 --user gpt-4o-mini --assistant-sliding-window 28000
+# dotenv run python test.py --assistant gpt-4o-mini --user gpt-4.1 --assistant-sliding-window 28000
+# dotenv run python test.py --assistant gpt-4.1-mini --user gpt-4.1 --assistant-sliding-window 28000
 dotenv run python test.py --assistant gpt-5-mini --user gpt-4.1 --assistant-sliding-window 28000
 dotenv run python test.py --assistant gpt-5 --user gpt-4.1 --assistant-sliding-window 28000
+dotenv run python test.py --assistant Qwen2.5-3B-Instruct --user gpt-4.1 --assistant-sliding-window 4000
+dotenv run python test.py --assistant gpt-4o --user gpt-4.1 --assistant-sliding-window 28000
