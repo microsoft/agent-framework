81308f905af2ab594a8d3143f0c89ec4316e2f69
Chris Rickman <crickman@microsoft.com>
2025-09-15 20:00:19 -0700

    Namespace

--- dotnet/src/Microsoft.Agents.Workflows.Declarative/PowerFx/WorkflowFormulaState.cs
+++ dotnet/src/Microsoft.Agents.Workflows.Declarative/PowerFx/WorkflowFormulaState.cs
@@ -2,7 +2,6 @@
 
 using System.Collections.Frozen;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
