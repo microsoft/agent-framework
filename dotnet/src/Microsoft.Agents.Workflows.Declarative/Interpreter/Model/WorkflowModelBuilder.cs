2187fd00763cd119cdbb1e32ae9833cb82c780ae
Chris Rickman <crickman@microsoft.com>
2025-09-24 08:19:23 -0700

    Address code-analysis issues

--- dotnet/src/Microsoft.Agents.Workflows.Declarative/Interpreter/Model/WorkflowModelBuilder.cs
+++ dotnet/src/Microsoft.Agents.Workflows.Declarative/Interpreter/Model/WorkflowModelBuilder.cs
@@ -19,12 +19,12 @@ internal sealed class WorkflowModelBuilder : IModelBuilder<Func<object?, bool>>
         Debug.WriteLine($"> CONNECT: {source.Id} => {target.Id}{(condition is null ? string.Empty : " (?)")}");
 
         this.WorkflowBuilder.AddEdge(
-            this.GetExecutorIsh(source),
-            this.GetExecutorIsh(target),
+            GetExecutorIsh(source),
+            GetExecutorIsh(target),
             condition);
     }
 
-    private ExecutorIsh GetExecutorIsh(IModeledAction action) =>
+    private static ExecutorIsh GetExecutorIsh(IModeledAction action) =>
         action switch
         {
             ModeledPort port => port.InputPort,
