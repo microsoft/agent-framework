fa870a4541b4cd6c9029fd27c5c6add3788389d0
Reuben Bond <reuben.bond@gmail.com>
2025-09-29 09:43:34 -0700

    Remove Actor-based runtime

--- dotnet/samples/AgentWebChat/AgentWebChat.AppHost/Program.cs
+++ dotnet/samples/AgentWebChat/AgentWebChat.AppHost/Program.cs
@@ -10,13 +10,9 @@ var chatModel = builder.AddAIModel("chat-model").AsAzureOpenAI("gpt-4o", o => o.
 
 var cosmosDbResource = builder.AddParameterFromConfiguration("CosmosDbName", "CosmosDb:Name");
 var cosmosDbResourceGroup = builder.AddParameterFromConfiguration("CosmosDbResourceGroup", "CosmosDb:ResourceGroup");
-var cosmos = builder.AddAzureCosmosDB("agent-web-chat-cosmosdb").RunAsExisting(cosmosDbResource, cosmosDbResourceGroup);
-
-var stateDb = cosmos.AddCosmosDatabase("actor-state-db");
 
 var agentHost = builder.AddProject<Projects.AgentWebChat_AgentHost>("agenthost")
-        .WithReference(chatModel)
-        .WithReference(cosmos).WaitFor(cosmos);
+        .WithReference(chatModel);
 
 builder.AddProject<Projects.AgentWebChat_Web>("webfrontend")
     .WithExternalHttpEndpoints()
