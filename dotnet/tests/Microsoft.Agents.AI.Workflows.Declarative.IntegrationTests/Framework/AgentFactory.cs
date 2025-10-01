2cff08d47b5da1661f948ea95814f3937a9e47f9
Chris Rickman <crickman@microsoft.com>
2025-10-01 12:33:04 -0700

    Namespace

--- dotnet/tests/Microsoft.Agents.AI.Workflows.Declarative.IntegrationTests/Framework/AgentFactory.cs
+++ dotnet/tests/Microsoft.Agents.AI.Workflows.Declarative.IntegrationTests/Framework/AgentFactory.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.AI.Agents.Persistent;
