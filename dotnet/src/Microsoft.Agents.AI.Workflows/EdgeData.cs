4c40697f37fc9dd7a187967cd5bc82ffe0299570
SergeyMenshykh <sergemenshikh@gmail.com>
2025-10-07 23:19:53 +0100

    use workflow info classes for workflow.definition tag value

--- dotnet/src/Microsoft.Agents.AI.Workflows/EdgeData.cs
+++ dotnet/src/Microsoft.Agents.AI.Workflows/EdgeData.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Text.Json.Serialization;
 using Microsoft.Agents.AI.Workflows.Execution;
 
 namespace Microsoft.Agents.AI.Workflows;
@@ -8,10 +7,6 @@ namespace Microsoft.Agents.AI.Workflows;
 /// <summary>
 /// A base class for edge data, providing access to the <see cref="EdgeConnection"/> representation of the edge.
 /// </summary>
-[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
-[JsonDerivedType(typeof(FanInEdgeData), typeDiscriminator: "fanIn")]
-[JsonDerivedType(typeof(FanOutEdgeData), typeDiscriminator: "fanOut")]
-[JsonDerivedType(typeof(DirectEdgeData), typeDiscriminator: "direct")]
 public abstract class EdgeData
 {
     /// <summary>
