60379332da09cca7b240f94ead713243fb970065
Korolev Dmitry <dmkorolev@microsoft.com>
2025-10-08 15:57:13 +0200

    rm unnecessary

--- dotnet/samples/AgentWebChat/AgentWebChat.Web/Program.cs
+++ dotnet/samples/AgentWebChat/AgentWebChat.Web/Program.cs
@@ -24,8 +24,8 @@ Uri a2aAddress = new("http://localhost:5390/a2a");
 builder.Services.AddHttpClient<AgentDiscoveryClient>(client => client.BaseAddress = baseAddress);
 builder.Services.AddSingleton(sp => new A2AAgentClient(sp.GetRequiredService<ILogger<A2AAgentClient>>(), a2aAddress));
 
-builder.Services.AddHttpClient<OpenAIResponsesAgentClient>(client => client.BaseAddress = baseAddress).AddServiceDiscovery();
-builder.Services.AddHttpClient<OpenAIChatCompletionsAgentClient>(client => client.BaseAddress = baseAddress).AddServiceDiscovery();
+builder.Services.AddHttpClient<OpenAIResponsesAgentClient>(client => client.BaseAddress = baseAddress);
+builder.Services.AddHttpClient<OpenAIChatCompletionsAgentClient>(client => client.BaseAddress = baseAddress);
 
 var app = builder.Build();
 
