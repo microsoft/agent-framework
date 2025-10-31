ece898ce52d0f33f554fda74a92bdb67e94b0c93
Chris Rickman <crickman@microsoft.com>
2025-10-31 12:12:41 -0700

    No build errors

--- dotnet/src/Microsoft.Agents.AI.Workflows.Declarative/AzureAgentProvider.cs
+++ dotnet/src/Microsoft.Agents.AI.Workflows.Declarative/AzureAgentProvider.cs
@@ -11,7 +11,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.AI.Agents;
 using Azure.Core;
-using Microsoft.Bot.ObjectModel;
 using Microsoft.Extensions.AI;
 using OpenAI.Responses;
 
@@ -27,29 +26,15 @@ namespace Microsoft.Agents.AI.Workflows.Declarative;
 /// <param name="httpClient">An optional <see cref="HttpClient"/> instance to be used for making HTTP requests. If not provided, a default client will be used.</param>
 public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential projectCredentials, HttpClient? httpClient = null) : WorkflowAgentProvider
 {
-    private static readonly Dictionary<string, AgentMessageRole> s_roleMap =
-        new()
-        {
-            [ChatRole.User.Value.ToUpperInvariant()] = AgentMessageRole.User,
-            [ChatRole.Assistant.Value.ToUpperInvariant()] = AgentMessageRole.Agent,
-            [ChatRole.System.Value.ToUpperInvariant()] = AgentMessageRole.Agent, // %%% ??!?!?!?!!!
-            [ChatRole.Tool.Value.ToUpperInvariant()] = AgentMessageRole.Agent, // %%% ??!?!?!?!!!
-        };
-
     private AgentsClient? _agentsClient;
 
     /// <inheritdoc/>
     public override async Task<string> CreateConversationAsync(CancellationToken cancellationToken = default)
     {
-        AgentConversationCreationOptions options =
-            new()
-            {
-                Items = { }, // %%% ??!?!?!?!!!
-                Metadata = { } // %%% ??!?!?!?!!!
-            };
-
         AgentConversation conversation =
-            await this.GetAgentsClient().GetConversationClient().CreateConversationAsync(options, cancellationToken).ConfigureAwait(false);
+            await this.GetAgentsClient()
+                .GetConversationClient()
+                .CreateConversationAsync(options: null, cancellationToken).ConfigureAwait(false);
 
         return conversation.Id;
     }
@@ -57,51 +42,16 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
     /// <inheritdoc/>
     public override async Task<ChatMessage> CreateMessageAsync(string conversationId, ChatMessage conversationMessage, CancellationToken cancellationToken = default)
     {
-        IList<ResponseItem> responseItems = [];
-        ReadOnlyCollection<ResponseItem> newMessages =
+        ChatMessage[] messages = [conversationMessage];
+
+        ReadOnlyCollection<ResponseItem> newItems =
             await this.GetAgentsClient().GetConversationClient().CreateConversationItemsAsync(
                 conversationId,
-                items: responseItems, // %%% ??!?!?!?!!!
+                items: messages.AsOpenAIResponseItems(),
                 include: null,
                 cancellationToken).ConfigureAwait(false);
 
-        //role: s_roleMap[conversationMessage.Role.Value.ToUpperInvariant()],
-        //contentBlocks: GetContent(),
-        //attachments: null,
-        //metadata: GetMetadata(),
-
-        return ToChatMessage(newMessages[0]); // %%% ??!?!?!?!!!
-
-        //Dictionary<string, string>? GetMetadata()
-        //{
-        //    if (conversationMessage.AdditionalProperties is null)
-        //    {
-        //        return null;
-        //    }
-
-        //    return conversationMessage.AdditionalProperties.ToDictionary(prop => prop.Key, prop => prop.Value?.ToString() ?? string.Empty);
-        //}
-
-        //IEnumerable<MessageInputContentBlock> GetContent()
-        //{
-        //    foreach (AIContent content in conversationMessage.Contents)
-        //    {
-        //        MessageInputContentBlock? contentBlock =
-        //            content switch
-        //            {
-        //                TextContent textContent => new MessageInputTextBlock(textContent.Text),
-        //                HostedFileContent fileContent => new MessageInputImageFileBlock(new MessageImageFileParam(fileContent.FileId)),
-        //                UriContent uriContent when uriContent.Uri is not null => new MessageInputImageUriBlock(new MessageImageUriParam(uriContent.Uri.ToString())),
-        //                DataContent dataContent when dataContent.Uri is not null => new MessageInputImageUriBlock(new MessageImageUriParam(dataContent.Uri)),
-        //                _ => null // Unsupported content type
-        //            };
-
-        //        if (contentBlock is not null)
-        //        {
-        //            yield return contentBlock;
-        //        }
-        //    }
-        //}
+        return newItems.AsChatMessages().Single(); // %%% BROKE ASSUMPTION - CARDINALITY
     }
 
     /// <inheritdoc/>
@@ -113,9 +63,13 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
                 agentId,
                 cancellationToken).ConfigureAwait(false);
 
-        //AllowMultipleToolCalls = this.AllowMultipleToolCalls,// %%% ??!?!?!?!!!
+        ChatOptions options =
+            new()
+            {
+                AllowMultipleToolCalls = this.AllowMultipleToolCalls,
+            };
 
-        AIAgent agent = client.GetAIAgent("gpt-4.1", agentRecord, options: null, clientFactory: null, openAIClientOptions: null, cancellationToken);
+        AIAgent agent = client.GetAIAgent(agentRecord, options, clientFactory: null, openAIClientOptions: null, cancellationToken);
 
         FunctionInvokingChatClient? functionInvokingClient = agent.GetService<FunctionInvokingChatClient>();
         if (functionInvokingClient is not null)
@@ -144,8 +98,9 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
     /// <inheritdoc/>
     public override async Task<ChatMessage> GetMessageAsync(string conversationId, string messageId, CancellationToken cancellationToken = default)
     {
-        AgentResponseItem message = await this.GetAgentsClient().GetConversationClient().GetConversationItemAsync(conversationId, messageId, cancellationToken).ConfigureAwait(false);
-        return ToChatMessage(message);
+        AgentResponseItem responseItem = await this.GetAgentsClient().GetConversationClient().GetConversationItemAsync(conversationId, messageId, cancellationToken).ConfigureAwait(false);
+        ResponseItem[] items = [responseItem.AsOpenAIResponseItem()];
+        return items.AsChatMessages().Single(); // %%% BROKE ASSUMPTION - CARDINALITY
     }
 
     /// <inheritdoc/>
@@ -158,9 +113,13 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         AgentsListOrder order = newestFirst ? AgentsListOrder.Asc : AgentsListOrder.Desc;
-        await foreach (AgentResponseItem message in this.GetAgentsClient().GetConversationClient().GetConversationItemsAsync(conversationId, limit, order, after, before, itemType: null, cancellationToken).ConfigureAwait(false))
+        await foreach (AgentResponseItem responseItem in this.GetAgentsClient().GetConversationClient().GetConversationItemsAsync(conversationId, limit, order, after, before, itemType: null, cancellationToken).ConfigureAwait(false))
         {
-            yield return ToChatMessage(message);
+            ResponseItem[] items = [responseItem.AsOpenAIResponseItem()];
+            foreach (ChatMessage message in items.AsChatMessages())
+            {
+                yield return message;
+            }
         }
     }
 
@@ -172,7 +131,7 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
 
             if (httpClient is not null)
             {
-                //clientOptions.Transport = new HttpClientTransport(httpClient); // %%% ??!?!?!?!!!
+                clientOptions.Transport = new HttpClientPipelineTransport(httpClient);
             }
 
             AgentsClient newClient = new(projectEndpoint, projectCredentials, clientOptions);
@@ -181,45 +140,5 @@ public sealed class AzureAgentProvider(Uri projectEndpoint, TokenCredential proj
         }
 
         return this._agentsClient;
-    }
-
-    private static ChatMessage ToChatMessage(AgentResponseItem message)
-    {
-        return
-           new ChatMessage(new ChatRole(message.Role.ToString()), [.. GetContent()])
-           {
-               MessageId = message.Id,
-               CreatedAt = message.CreatedAt,
-               AdditionalProperties = GetMetadata()
-           };
-
-        IEnumerable<AIContent> GetContent()
-        {
-            foreach (MessageContent contentItem in message.ContentItems)
-            {
-                AIContent? content =
-                    contentItem switch
-                    {
-                        MessageTextContent textContent => new TextContent(textContent.Text),
-                        MessageImageFileContent imageContent => new HostedFileContent(imageContent.FileId),
-                        _ => null // Unsupported content type
-                    };
-
-                if (content is not null)
-                {
-                    yield return content;
-                }
-            }
-        }
-
-        AdditionalPropertiesDictionary? GetMetadata()
-        {
-            if (message.Metadata is null)
-            {
-                return null;
-            }
-
-            return new AdditionalPropertiesDictionary(message.Metadata.Select(m => new KeyValuePair<string, object?>(m.Key, m.Value)));
-        }
     }
 }
