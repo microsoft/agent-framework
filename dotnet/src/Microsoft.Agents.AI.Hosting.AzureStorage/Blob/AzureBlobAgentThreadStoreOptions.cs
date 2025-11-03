18b08567481fa8a6ba5ab17a9664276173d2cf79
Korolev Dmitry <dmkorolev@microsoft.com>
2025-11-03 19:19:14 +0100

    setup azurestorage proj

---
+++ dotnet/src/Microsoft.Agents.AI.Hosting.AzureStorage/Blob/AzureBlobAgentThreadStoreOptions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Agents.AI.Hosting.AzureStorage.Blob;
+
+/// <summary>
+/// Configuration options for <see cref="AzureBlobAgentThreadStore"/>.
+/// </summary>
+public sealed class AzureBlobAgentThreadStoreOptions
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether to automatically create the container if it doesn't exist.
+    /// </summary>
+    /// <remarks>
+    /// Defaults to <see langword="true"/>.
+    /// </remarks>
+    public bool CreateContainerIfNotExists { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the blob name prefix to use for organizing threads.
+    /// </summary>
+    /// <remarks>
+    /// This can be used to namespace threads within a container.
+    /// For example, setting this to "prod/" will store all blobs under a "prod/" prefix.
+    /// </remarks>
+    public string? BlobNamePrefix { get; set; }
+}
---
+++ dotnet/src/Microsoft.Agents.AI.Hosting.AzureStorage/HostedAgentBuilderExtensions.Blob.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Azure.Storage.Blobs;
+using Microsoft.Agents.AI.Hosting.AzureStorage.Blob;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Agents.AI.Hosting;
+
+/// <summary>
+/// Provides extension methods for configuring <see cref="AIAgent"/>.
+/// </summary>
+public static partial class HostedAgentBuilderExtensions
+{
+    /// <summary>
+    /// Configures the host agent builder to use an Azure Blob thread store with dependency injection.
+    /// Resolves <see cref="BlobContainerClient"/> from the service provider.
+    /// </summary>
+    /// <param name="builder">The host agent builder to configure with the Azure blob thread store.</param>
+    /// <param name="options">Optional configuration options for the blob thread store.</param>
+    /// <returns>The same <paramref name="builder"/> instance, configured to use Azure blob thread store.</returns>
+    /// <remarks>
+    /// This overload requires a <see cref="BlobContainerClient"/> to be registered in the service collection.
+    /// Use Azure.Extensions.AspNetCore.Configuration.Secrets or similar to register the client.
+    /// </remarks>
+    public static IHostedAgentBuilder WithAzureBlobThreadStore(this IHostedAgentBuilder builder, AzureBlobAgentThreadStoreOptions? options = null)
+        => WithAzureBlobThreadStore(builder, sp => sp.GetRequiredKeyedService<BlobContainerClient>(builder.Name), options);
+
+    /// <summary>
+    /// Configures the host agent builder to use an Azure Blob thread store for agent thread management.
+    /// </summary>
+    /// <param name="builder">The host agent builder to configure with the Azure blob thread store.</param>
+    /// <param name="containerClient">The blob container client to use for storage operations.</param>
+    /// <param name="options">Optional configuration options for the blob thread store.</param>
+    /// <returns>The same <paramref name="builder"/> instance, configured to use Azure blob thread store.</returns>
+    public static IHostedAgentBuilder WithAzureBlobThreadStore(this IHostedAgentBuilder builder, BlobContainerClient containerClient, AzureBlobAgentThreadStoreOptions? options = null)
+        => WithAzureBlobThreadStore(builder, sp => containerClient, options);
+
+    /// <summary>
+    /// Configures the agent builder to use Azure Blob Storage as the thread store for agent state persistence.
+    /// </summary>
+    /// <param name="builder">The agent builder to configure with Azure Blob thread store support.</param>
+    /// <param name="createBlobContainer">A factory function that provides a configured BlobContainerClient instance for accessing the Azure Blob
+    /// container used to store thread data.</param>
+    /// <param name="options">Optional settings for customizing the Azure Blob thread store behavior. If null, default options are used.</param>
+    /// <returns>The same agent builder instance, configured to use Azure Blob Storage for thread persistence.</returns>
+    public static IHostedAgentBuilder WithAzureBlobThreadStore(
+        this IHostedAgentBuilder builder,
+        Func<IServiceProvider, BlobContainerClient> createBlobContainer,
+        AzureBlobAgentThreadStoreOptions? options = null)
+            => builder.WithThreadStore((sp, key) =>
+            {
+                var blobContainer = createBlobContainer(sp);
+                return new AzureBlobAgentThreadStore(blobContainer, options);
+            });
+}
