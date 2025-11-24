8afb09400a7a006ee9d61bf98c09e4f5e666d237
Chris Rickman <crickman@microsoft.com>
2025-11-24 12:42:10 -0800

    Empty framework

---
+++ dotnet/src/Microsoft.Agents.AI.Workflows.Declarative.OpenAI/OpenAIChatAgentProvider.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace Microsoft.Agents.AI.Workflows.Declarative;
+
+/// <summary>
+/// $$$ COMMENT
+/// </summary>
+/// <param name="client">The service client.</param>
+public sealed class OpenAIChatAgentProvider(OpenAIClient client) : WorkflowAgentProvider
+{
+    /// <summary>
+    /// The OpenAI client used to interact with the OpenAI service.
+    /// </summary>
+    public OpenAIClient Client { get; } = client;
+
+    /// <inheritdoc/>
+    public override async Task<string> CreateConversationAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override async Task<ChatMessage> CreateMessageAsync(string conversationId, ChatMessage conversationMessage, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override IAsyncEnumerable<AgentRunResponseUpdate> InvokeAgentAsync(
+        string agentId,
+        string? agentVersion,
+        string? conversationId,
+        IEnumerable<ChatMessage>? messages,
+        IDictionary<string, object?>? inputArguments,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override async Task<ChatMessage> GetMessageAsync(string conversationId, string messageId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public override IAsyncEnumerable<ChatMessage> GetMessagesAsync(
+        string conversationId,
+        int? limit = null,
+        string? after = null,
+        string? before = null,
+        bool newestFirst = false,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
