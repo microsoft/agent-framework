61055027d3ae15fcae4a04276690e07795c1f80b
Sunil Dutta <sunil.dutta@penske.com>
2025-12-03 12:50:12 -0500

    Pushing the bedrock related changes to the new branch after addressing the review comments

---
+++ python/packages/core/agent_framework/amazon/__init__.pyi
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from agent_framework_bedrock import (
+    BedrockChatClient,
+    __version__,
+)
+
+__all__ = [
+    "BedrockChatClient",
+    "__version__",
+]
