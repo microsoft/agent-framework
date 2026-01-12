2ba1a257a54ebd57e4fa9fede20f15f923cae52e
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2026-01-12 14:23:42 +0000

    Start to fix compile errors due to refactoring

--- dotnet/src/Microsoft.Agents.AI.Declarative.AzureAI.Persistent/FoundryAgentFactory.cs
+++ dotnet/src/Microsoft.Agents.AI.Declarative.AzureAI.Persistent/FoundryAgentFactory.cs
@@ -2,29 +2,30 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure.AI.Agents;
 using Azure.AI.Agents.Persistent;
 using Azure.Core;
 using Microsoft.Bot.ObjectModel;
 using Microsoft.Extensions.Configuration;
+using Microsoft.PowerFx;
 using Microsoft.Shared.Diagnostics;
 
 namespace Microsoft.Agents.AI;
 
 /// <summary>
-/// Provides an <see cref="AgentFactory"/> which creates instances of <see cref="AIAgent"/> using a <see cref="AgentClient"/>.
+/// Provides an <see cref="PromptAgentFactory"/> which creates instances of <see cref="AIAgent"/> using a <see cref="PersistentAgentsClient"/>.
 /// </summary>
-public sealed class FoundryAgentFactory : AgentFactory
+public sealed class FoundryAgentFactory : PromptAgentFactory
 {
-    private readonly AgentClient? _agentClient;
+    private readonly PersistentAgentsClient? _agentClient;
     private readonly TokenCredential? _tokenCredential;
 
     /// <summary>
-    /// Creates a new instance of the <see cref="FoundryAgentFactory"/> class with an associated <see cref="AgentClient"/>.
+    /// Creates a new instance of the <see cref="FoundryAgentFactory"/> class with an associated <see cref="PersistentAgentsClient"/>.
     /// </summary>
-    /// <param name="agentClient">The <see cref="AgentClient"/> instance to use for creating agents.</param>
+    /// <param name="agentClient">The <see cref="PersistentAgentsClient"/> instance to use for creating agents.</param>
+    /// <param name="engine">Optional <see cref="RecalcEngine"/>, if none is provided a default instance will be created.</param>
     /// <param name="configuration">The <see cref="IConfiguration"/> instance to use for configuration.</param>
-    public FoundryAgentFactory(AgentClient agentClient, IConfiguration? configuration = null) : base(configuration)
+    public FoundryAgentFactory(PersistentAgentsClient agentClient, RecalcEngine? engine = null, IConfiguration? configuration = null) : base(engine, configuration)
     {
         Throw.IfNull(agentClient);
 
@@ -35,8 +36,9 @@ public sealed class FoundryAgentFactory : AgentFactory
     /// Creates a new instance of the <see cref="FoundryAgentFactory"/> class with an associated <see cref="TokenCredential"/>.
     /// </summary>
     /// <param name="tokenCredential">The <see cref="TokenCredential"/> to use for authenticating requests.</param>
+    /// <param name="engine">Optional <see cref="RecalcEngine"/>, if none is provided a default instance will be created.</param>
     /// <param name="configuration">The <see cref="IConfiguration"/> instance to use for configuration.</param>
-    public FoundryAgentFactory(TokenCredential tokenCredential, IConfiguration? configuration = null) : base(configuration)
+    public FoundryAgentFactory(TokenCredential tokenCredential, RecalcEngine? engine = null, IConfiguration? configuration = null) : base(engine, configuration)
     {
         Throw.IfNull(tokenCredential);
 
@@ -58,6 +60,7 @@ public sealed class FoundryAgentFactory : AgentFactory
 
         var modelOptions = promptAgent.Model?.Options;
 
+        /*
         var promptAgentDefinition = new PromptAgentDefinition(model: modelId)
         {
             Instructions = promptAgent.Instructions?.ToTemplateString(),
@@ -84,9 +87,25 @@ public sealed class FoundryAgentFactory : AgentFactory
         var agentVersion = await agentClient.CreateAgentVersionAsync(agentName: promptAgent.Name, options: agentVersionCreationOptions, cancellationToken: cancellationToken).ConfigureAwait(false);
 
         return agentClient.GetAIAgent(agentVersion, cancellationToken: cancellationToken);
+        */
+
+        var createPersistentAgentResponse = agentClient.Administration.CreateAgent(
+            model: modelId,
+            name: promptAgent.Name,
+            instructions: promptAgent.Instructions?.ToTemplateString(),
+            tools: tools,
+            toolResources: toolResources,
+            temperature: (float?)modelOptions?.Temperature?.LiteralValue,
+            topP: (float?)modelOptions?.TopP?.LiteralValue,
+            responseFormat: responseFormat,
+            metadata: promptAgent.Metadata?.ToDictionary(),
+            cancellationToken: cancellationToken);
+
+        // Get a local proxy for the agent to work with.
+        return agentClient.GetAIAgent(createPersistentAgentResponse.Value.Id, clientFactory: null, services: null, cancellationToken: cancellationToken);
     }
 
-    private AgentClient CreateAgentClient(GptComponentMetadata promptAgent)
+    private PersistentAgentsClient CreateAgentClient(GptComponentMetadata promptAgent)
     {
         var externalModel = promptAgent.Model as CurrentModels;
         var connection = externalModel?.Connection as RemoteConnection;
@@ -103,9 +122,9 @@ public sealed class FoundryAgentFactory : AgentFactory
                 throw new InvalidOperationException("A TokenCredential must be registered in the service provider to create an AgentClient.");
             }
 
-            return new AgentClient(new Uri(endpoint), this._tokenCredential);
+            return new PersistentAgentsClient(endpoint, this._tokenCredential);
         }
 
-        throw new InvalidOperationException("An AgentClient must be registered in the service provider or a RemoteConnection must be specified in the agent definition model connection to create an AgentClient.");
+        throw new InvalidOperationException("A PersistentAgentsClient must be registered in the service provider or a RemoteConnection must be specified in the agent definition model connection to create a PersistentAgentsClient.");
     }
 }
