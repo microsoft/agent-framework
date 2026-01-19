685adf01c77e19403c356cfc4c5acbc9b57b3fcb
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2026-01-18 23:40:21 -0800

    Small fix

---
+++ python/packages/core/agent_framework/shell_local/__init__.pyi
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from agent_framework_shell_local import LocalShellExecutor, __version__
+
+__all__ = [
+    "LocalShellExecutor",
+    "__version__",
+]
