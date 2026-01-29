bc67df98c8d3cec44107171dc537a2cded39b60a
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2026-01-29 13:28:22 -0800

    Small update

--- python/packages/core/agent_framework/anthropic/__init__.pyi
+++ python/packages/core/agent_framework/anthropic/__init__.pyi
@@ -1,19 +1,13 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from agent_framework_anthropic import (
-    AnthropicChatOptions as AnthropicChatOptions,
-)
-from agent_framework_anthropic import (
-    AnthropicClient as AnthropicClient,
-)
-from agent_framework_anthropic import (
-    __version__ as __version__,
+    AnthropicChatOptions,
+    AnthropicClient,
+    __version__,
 )
 from agent_framework_claude import (
-    ClaudeAgent as ClaudeAgent,
-)
-from agent_framework_claude import (
-    ClaudeAgentOptions as ClaudeAgentOptions,
+    ClaudeAgent,
+    ClaudeAgentOptions,
 )
 
 __all__ = [
