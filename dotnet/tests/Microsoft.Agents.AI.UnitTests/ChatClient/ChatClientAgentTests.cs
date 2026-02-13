b77dbbf85c2b0b1c51ad5f9f318b1910d9d1567b
SergeyMenshykh <sergemenshikh@gmail.com>
2026-02-13 15:09:47 +0000

    forgotten change

--- dotnet/tests/Microsoft.Agents.AI.UnitTests/ChatClient/ChatClientAgentTests.cs
+++ dotnet/tests/Microsoft.Agents.AI.UnitTests/ChatClient/ChatClientAgentTests.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.AI;
