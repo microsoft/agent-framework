rogerbarreto/agent-framework-public

Simplify Foundry agents and validate moved samples

dotnet/samples/02-agents/AgentsWithFoundry/Versioned/Agent_Step04_UsingFunctionToolsWithApprovals/Program.cs
@@ -1,0 +1,67 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+// This sample demonstrates how to use an agent with function tools that require a human in the loop for approvals.
+// It shows both non-streaming and streaming agent interactions using weather-related tools.
+// If the agent is hosted in a service, with a remote user, combine this sample with the Persisted Conversations sample to persist the chat history
+// while the agent is waiting for user input.
+
+using System.ComponentModel;
+using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Responses;
+
+// Create a sample function tool that the agent can use.
+[Description("Get the weather for a given location.")]
+static string GetWeather([Description("The location to get the weather for.")] string location)
+    => $"The weather in {location} is cloudy with a high of 15°C.";
+
+string endpoint = Environment.GetEnvironmentVariable("AZURE_AI_PROJECT_ENDPOINT") ?? throw new InvalidOperationException("AZURE_AI_PROJECT_ENDPOINT is not set.");
+string deploymentName = Environment.GetEnvironmentVariable("AZURE_AI_MODEL_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+const string AssistantInstructions = "You are a helpful assistant that can get weather information.";
+const string AssistantName = "WeatherAssistant";
+AIProjectClient aiProjectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+
+ApprovalRequiredAIFunction approvalTool = new(AIFunctionFactory.Create(GetWeather, name: nameof(GetWeather)));
+
+PromptAgentDefinition agentDefinition = new(model: deploymentName)
+{
+    Instructions = AssistantInstructions,
+    Tools = { approvalTool.GetService<ResponseTool>() ?? approvalTool.AsOpenAIResponseTool() ?? throw new InvalidOperationException("Unable to convert approval tool to a ResponseTool.") }
+};
+AgentVersion agentVersion = await aiProjectClient.Agents.CreateAgentVersionAsync(AssistantName, new AgentVersionCreationOptions(agentDefinition));
+ChatClientAgent agent = aiProjectClient.AsAIAgent(agentVersion, [approvalTool]);
+
+// Call the agent with approval-required function tools.
+// The agent will request approval before invoking the function.
+AgentSession session = await agent.CreateSessionAsync();
+AgentResponse response = await agent.RunAsync("What is the weather like in Amsterdam?", session);
+
+// Check if there are any approval requests.
+// For simplicity, we are assuming here that only function approvals are pending.
+List<FunctionApprovalRequestContent> approvalRequests = response.Messages.SelectMany(m => m.Contents).OfType<FunctionApprovalRequestContent>().ToList();
+
+while (approvalRequests.Count > 0)
+{
+    // Ask the user to approve each function call request.
+    List<ChatMessage> userInputMessages = approvalRequests
+        .ConvertAll(functionApprovalRequest =>
+        {
+            Console.WriteLine($"The agent would like to invoke the following function, please reply Y to approve: Name {functionApprovalRequest.FunctionCall.Name}");
+            bool approved = Console.ReadLine()?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false;
+            return new ChatMessage(ChatRole.User, [functionApprovalRequest.CreateResponse(approved)]);
+        });
+
+    // Pass the user input responses back to the agent for further processing.
+    response = await agent.RunAsync(userInputMessages, session);
+
+    approvalRequests = response.Messages.SelectMany(m => m.Contents).OfType<FunctionApprovalRequestContent>().ToList();
+}
+
+Console.WriteLine($"\nAgent: {response}");
+
+// Cleanup: deletes the agent and all its versions.
+await aiProjectClient.Agents.DeleteAgentAsync(agent.Name);

dotnet/samples/02-agents/AgentsWithFoundry/Versioned/Agent_Step14_CodeInterpreter/Program.cs
@@ -1,0 +1,92 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+// This sample shows how to use Code Interpreter Tool with AI Agents.
+
+using System.Text;
+using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Assistants;
+using OpenAI.Responses;
+
+string endpoint = Environment.GetEnvironmentVariable("AZURE_AI_PROJECT_ENDPOINT") ?? throw new InvalidOperationException("AZURE_AI_PROJECT_ENDPOINT is not set.");
+string deploymentName = Environment.GetEnvironmentVariable("AZURE_AI_MODEL_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+const string AgentInstructions = "You are a personal math tutor. When asked a math question, write and run code using the python tool to answer the question.";
+const string AgentNameMEAI = "CoderAgent-MEAI";
+const string AgentNameNative = "CoderAgent-NATIVE";
+AIProjectClient aiProjectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+
+// Option 1 - Using HostedCodeInterpreterTool + AgentOptions (MEAI + AgentFramework)
+HostedCodeInterpreterTool hostedCodeInterpreterTool = new() { Inputs = [] };
+AgentVersion agentVersion1 = await aiProjectClient.Agents.CreateAgentVersionAsync(
+    AgentNameMEAI,
+    new AgentVersionCreationOptions(
+        new PromptAgentDefinition(model: deploymentName)
+        {
+            Instructions = AgentInstructions,
+            Tools = { hostedCodeInterpreterTool.GetService<ResponseTool>() ?? hostedCodeInterpreterTool.AsOpenAIResponseTool() ?? throw new InvalidOperationException("Unable to convert hosted code interpreter tool to a ResponseTool.") }
+        }));
+ChatClientAgent agentOption1 = aiProjectClient.AsAIAgent(agentVersion1);
+
+// Option 2 - Using PromptAgentDefinition SDK native type
+// Create the server side agent version
+AgentVersion agentVersion2 = await aiProjectClient.Agents.CreateAgentVersionAsync(
+    AgentNameNative,
+    new AgentVersionCreationOptions(
+        new PromptAgentDefinition(model: deploymentName)
+        {
+            Instructions = AgentInstructions,
+            Tools = {
+                ResponseTool.CreateCodeInterpreterTool(
+                    new CodeInterpreterToolContainer(
+                        CodeInterpreterToolContainerConfiguration.CreateAutomaticContainerConfiguration(fileIds: [])
+                    )
+                ),
+            }
+        }));
+ChatClientAgent agentOption2 = aiProjectClient.AsAIAgent(agentVersion2);
+
+// Either invoke option1 or option2 agent, should have same result
+// Option 1
+AgentResponse response = await agentOption1.RunAsync("I need to solve the equation sin(x) + x^2 = 42");
+
+// Option 2
+// AgentResponse response = await agentOption2.RunAsync("I need to solve the equation sin(x) + x^2 = 42");
+
+// Get the CodeInterpreterToolCallContent
+CodeInterpreterToolCallContent? toolCallContent = response.Messages.SelectMany(m => m.Contents).OfType<CodeInterpreterToolCallContent>().FirstOrDefault();
+if (toolCallContent?.Inputs is not null)
+{
+    DataContent? codeInput = toolCallContent.Inputs.OfType<DataContent>().FirstOrDefault();
+    if (codeInput?.HasTopLevelMediaType("text") ?? false)
+    {
+        Console.WriteLine($"Code Input: {Encoding.UTF8.GetString(codeInput.Data.ToArray()) ?? "Not available"}");
+    }
+}
+
+// Get the CodeInterpreterToolResultContent
+CodeInterpreterToolResultContent? toolResultContent = response.Messages.SelectMany(m => m.Contents).OfType<CodeInterpreterToolResultContent>().FirstOrDefault();
+if (toolResultContent?.Outputs is not null && toolResultContent.Outputs.OfType<TextContent>().FirstOrDefault() is { } resultOutput)
+{
+    Console.WriteLine($"Code Tool Result: {resultOutput.Text}");
+}
+
+// Getting any annotations generated by the tool
+foreach (AIAnnotation annotation in response.Messages.SelectMany(m => m.Contents).SelectMany(C => C.Annotations ?? []))
+{
+    if (annotation.RawRepresentation is TextAnnotationUpdate citationAnnotation)
+    {
+        Console.WriteLine($$"""
+            File Id: {{citationAnnotation.OutputFileId}}
+            Text to Replace: {{citationAnnotation.TextToReplace}}
+            Filename: {{Path.GetFileName(citationAnnotation.TextToReplace)}}
+            """);
+    }
+}
+
+// Cleanup: deletes the agent and all its versions.
+await aiProjectClient.Agents.DeleteAgentAsync(agentOption1.Name);
+await aiProjectClient.Agents.DeleteAgentAsync(agentOption2.Name);

microsoft/agent-framework

dotnet/samples/02-agents/AgentsWithFoundry/Versioned/Agent_Step04_UsingFunctionToolsWithApprovals/Program.cs
@@ -1,0 +1,67 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+// This sample demonstrates how to use an agent with function tools that require a human in the loop for approvals.
+// It shows both non-streaming and streaming agent interactions using weather-related tools.
+// If the agent is hosted in a service, with a remote user, combine this sample with the Persisted Conversations sample to persist the chat history
+// while the agent is waiting for user input.
+
+using System.ComponentModel;
+using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Responses;
+
+// Create a sample function tool that the agent can use.
+[Description("Get the weather for a given location.")]
+static string GetWeather([Description("The location to get the weather for.")] string location)
+    => $"The weather in {location} is cloudy with a high of 15°C.";
+
+string endpoint = Environment.GetEnvironmentVariable("AZURE_AI_PROJECT_ENDPOINT") ?? throw new InvalidOperationException("AZURE_AI_PROJECT_ENDPOINT is not set.");
+string deploymentName = Environment.GetEnvironmentVariable("AZURE_AI_MODEL_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+const string AssistantInstructions = "You are a helpful assistant that can get weather information.";
+const string AssistantName = "WeatherAssistant";
+AIProjectClient aiProjectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+
+ApprovalRequiredAIFunction approvalTool = new(AIFunctionFactory.Create(GetWeather, name: nameof(GetWeather)));
+
+PromptAgentDefinition agentDefinition = new(model: deploymentName)
+{
+    Instructions = AssistantInstructions,
+    Tools = { approvalTool.GetService<ResponseTool>() ?? approvalTool.AsOpenAIResponseTool() ?? throw new InvalidOperationException("Unable to convert approval tool to a ResponseTool.") }
+};
+AgentVersion agentVersion = await aiProjectClient.Agents.CreateAgentVersionAsync(AssistantName, new AgentVersionCreationOptions(agentDefinition));
+ChatClientAgent agent = aiProjectClient.AsAIAgent(agentVersion, [approvalTool]);
+
+// Call the agent with approval-required function tools.
+// The agent will request approval before invoking the function.
+AgentSession session = await agent.CreateSessionAsync();
+AgentResponse response = await agent.RunAsync("What is the weather like in Amsterdam?", session);
+
+// Check if there are any approval requests.
+// For simplicity, we are assuming here that only function approvals are pending.
+List<ToolApprovalRequestContent> approvalRequests = response.Messages.SelectMany(m => m.Contents).OfType<ToolApprovalRequestContent>().ToList();
+
+while (approvalRequests.Count > 0)
+{
+    // Ask the user to approve each function call request.
+    List<ChatMessage> userInputMessages = approvalRequests
+        .ConvertAll(functionApprovalRequest =>
+        {
+            Console.WriteLine($"The agent would like to invoke the following function, please reply Y to approve: Name {((FunctionCallContent)functionApprovalRequest.ToolCall).Name}");
+            bool approved = Console.ReadLine()?.Equals("Y", StringComparison.OrdinalIgnoreCase) ?? false;
+            return new ChatMessage(ChatRole.User, [functionApprovalRequest.CreateResponse(approved)]);
+        });
+
+    // Pass the user input responses back to the agent for further processing.
+    response = await agent.RunAsync(userInputMessages, session);
+
+    approvalRequests = response.Messages.SelectMany(m => m.Contents).OfType<ToolApprovalRequestContent>().ToList();
+}
+
+Console.WriteLine($"\nAgent: {response}");
+
+// Cleanup: deletes the agent and all its versions.
+await aiProjectClient.Agents.DeleteAgentAsync(agent.Name);

dotnet/samples/02-agents/AgentsWithFoundry/Versioned/Agent_Step14_CodeInterpreter/Program.cs
@@ -1,0 +1,92 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+// This sample shows how to use Code Interpreter Tool with AI Agents.
+
+using System.Text;
+using Azure.AI.Projects;
+using Azure.AI.Projects.Agents;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Assistants;
+using OpenAI.Responses;
+
+string endpoint = Environment.GetEnvironmentVariable("AZURE_AI_PROJECT_ENDPOINT") ?? throw new InvalidOperationException("AZURE_AI_PROJECT_ENDPOINT is not set.");
+string deploymentName = Environment.GetEnvironmentVariable("AZURE_AI_MODEL_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+const string AgentInstructions = "You are a personal math tutor. When asked a math question, write and run code using the python tool to answer the question.";
+const string AgentNameMEAI = "CoderAgent-MEAI";
+const string AgentNameNative = "CoderAgent-NATIVE";
+AIProjectClient aiProjectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+
+// Option 1 - Using HostedCodeInterpreterTool + AgentOptions (MEAI + AgentFramework)
+HostedCodeInterpreterTool hostedCodeInterpreterTool = new() { Inputs = [] };
+AgentVersion agentVersion1 = await aiProjectClient.Agents.CreateAgentVersionAsync(
+    AgentNameMEAI,
+    new AgentVersionCreationOptions(
+        new PromptAgentDefinition(model: deploymentName)
+        {
+            Instructions = AgentInstructions,
+            Tools = { hostedCodeInterpreterTool.GetService<ResponseTool>() ?? hostedCodeInterpreterTool.AsOpenAIResponseTool() ?? throw new InvalidOperationException("Unable to convert hosted code interpreter tool to a ResponseTool.") }
+        }));
+ChatClientAgent agentOption1 = aiProjectClient.AsAIAgent(agentVersion1);
+
+// Option 2 - Using PromptAgentDefinition SDK native type
+// Create the server side agent version
+AgentVersion agentVersion2 = await aiProjectClient.Agents.CreateAgentVersionAsync(
+    AgentNameNative,
+    new AgentVersionCreationOptions(
+        new PromptAgentDefinition(model: deploymentName)
+        {
+            Instructions = AgentInstructions,
+            Tools = {
+                ResponseTool.CreateCodeInterpreterTool(
+                    new CodeInterpreterToolContainer(
+                        CodeInterpreterToolContainerConfiguration.CreateAutomaticContainerConfiguration(fileIds: [])
+                    )
+                ),
+            }
+        }));
+ChatClientAgent agentOption2 = aiProjectClient.AsAIAgent(agentVersion2);
+
+// Either invoke option1 or option2 agent, should have same result
+// Option 1
+AgentResponse response = await agentOption1.RunAsync("I need to solve the equation sin(x) + x^2 = 42");
+
+// Option 2
+// AgentResponse response = await agentOption2.RunAsync("I need to solve the equation sin(x) + x^2 = 42");
+
+// Get the CodeInterpreterToolCallContent
+CodeInterpreterToolCallContent? toolCallContent = response.Messages.SelectMany(m => m.Contents).OfType<CodeInterpreterToolCallContent>().FirstOrDefault();
+if (toolCallContent?.Inputs is not null)
+{
+    DataContent? codeInput = toolCallContent.Inputs.OfType<DataContent>().FirstOrDefault();
+    if (codeInput?.HasTopLevelMediaType("text") ?? false)
+    {
+        Console.WriteLine($"Code Input: {Encoding.UTF8.GetString(codeInput.Data.ToArray()) ?? "Not available"}");
+    }
+}
+
+// Get the CodeInterpreterToolResultContent
+CodeInterpreterToolResultContent? toolResultContent = response.Messages.SelectMany(m => m.Contents).OfType<CodeInterpreterToolResultContent>().FirstOrDefault();
+if (toolResultContent?.Outputs is not null && toolResultContent.Outputs.OfType<TextContent>().FirstOrDefault() is { } resultOutput)
+{
+    Console.WriteLine($"Code Tool Result: {resultOutput.Text}");
+}
+
+// Getting any annotations generated by the tool
+foreach (AIAnnotation annotation in response.Messages.SelectMany(m => m.Contents).SelectMany(C => C.Annotations ?? []))
+{
+    if (annotation.RawRepresentation is TextAnnotationUpdate citationAnnotation)
+    {
+        Console.WriteLine($$"""
+            File Id: {{citationAnnotation.OutputFileId}}
+            Text to Replace: {{citationAnnotation.TextToReplace}}
+            Filename: {{Path.GetFileName(citationAnnotation.TextToReplace)}}
+            """);
+    }
+}
+
+// Cleanup: deletes the agent and all its versions.
+await aiProjectClient.Agents.DeleteAgentAsync(agentOption1.Name);
+await aiProjectClient.Agents.DeleteAgentAsync(agentOption2.Name);