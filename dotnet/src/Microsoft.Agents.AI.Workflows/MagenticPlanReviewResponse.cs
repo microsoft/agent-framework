1c0c6483e82138070f7a87807fa5afb00217d727
Jacob Alber <jaalber@microsoft.com>
2026-04-30 15:51:57 -0400

    feat: Implement Magentic Orchestration for .NET

---
+++ dotnet/src/Microsoft.Agents.AI.Workflows/MagenticPlanReviewResponse.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Workflows;
+
+/// <summary>
+/// .
+/// </summary>
+/// <param name="Review"></param>
+public record MagenticPlanReviewResponse(List<ChatMessage> Review)
+{
+    internal bool IsApproved => this.Review.Count == 0;
+}
