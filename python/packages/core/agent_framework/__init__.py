833ac5607babbee283c7d9e13eff517eb55d55d6
venti <1308199824@qq.com>
2026-05-30 15:03:04 +0800

    Python: add ChatMessageStore backward-compat alias for HistoryProvider (fixes #5700)

--- python/packages/core/agent_framework/__init__.py
+++ python/packages/core/agent_framework/__init__.py
@@ -153,6 +153,7 @@ from ._sessions import (
     SessionContext,
     register_state_type,
 )
+ChatMessageStore = HistoryProvider
 from ._settings import SecretString, load_settings
 from ._skills import (
     AggregatingSkillsSource,
@@ -371,6 +372,7 @@ __all__ = [
     "ChatMiddleware",
     "ChatMiddlewareLayer",
     "ChatMiddlewareTypes",
+    "ChatMessageStore",
     "ChatOptions",
     "ChatResponse",
     "ChatResponseUpdate",
