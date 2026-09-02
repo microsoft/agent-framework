755c4c073dd47d967861800590584def8701926e
UniversePeak <113168673+UniversePeak@users.noreply.github.com>
2026-09-03 06:03:06 +0800

    Fix hosted workflow responses to include outputs

--- dotnet/src/Microsoft.Agents.AI.Hosting/HostedWorkflowBuilderExtensions.cs
+++ dotnet/src/Microsoft.Agents.AI.Hosting/HostedWorkflowBuilderExtensions.cs
@@ -15,6 +15,7 @@ public static class HostedWorkflowBuilderExtensions
     /// </summary>
     /// <param name="builder">The <see cref="IHostedWorkflowBuilder"/> instance to extend.</param>
     /// <param name="lifetime">The DI service lifetime for the agent registration. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
+    /// <remarks>Workflow outputs are included in the hosted agent response.</remarks>
     /// <returns>An <see cref="IHostedAgentBuilder"/> that can be used to further configure the agent.</returns>
     public static IHostedAgentBuilder AddAsAIAgent(this IHostedWorkflowBuilder builder, ServiceLifetime lifetime = ServiceLifetime.Singleton)
         => builder.AddAsAIAgent(name: null, lifetime: lifetime);
@@ -25,6 +26,7 @@ public static class HostedWorkflowBuilderExtensions
     /// <param name="builder">The <see cref="IHostedWorkflowBuilder"/> instance to extend.</param>
     /// <param name="name">The optional name for the AI agent. If not specified, the workflow name is used.</param>
     /// <param name="lifetime">The DI service lifetime for the agent registration. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
+    /// <remarks>Workflow outputs are included in the hosted agent response.</remarks>
     /// <returns>An <see cref="IHostedAgentBuilder"/> that can be used to further configure the agent.</returns>
     public static IHostedAgentBuilder AddAsAIAgent(this IHostedWorkflowBuilder builder, string? name, ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
@@ -32,6 +34,8 @@ public static class HostedWorkflowBuilderExtensions
         var agentName = name ?? workflowName;
 
         return builder.HostApplicationBuilder.AddAIAgent(agentName, (sp, key) =>
-            sp.GetRequiredKeyedService<Workflow>(workflowName).AsAIAgent(name: key), lifetime);
+            sp.GetRequiredKeyedService<Workflow>(workflowName).AsAIAgent(
+                name: key,
+                includeWorkflowOutputsInResponse: true), lifetime);
     }
 }
